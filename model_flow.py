e97795f2dea915e8e7c844780905bae5ef859081
taekuyng kang <douit_be@naver.com>
2021-08-15 18:29:05 +0900

    model_flow

---
+++ model_flow.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+from torchvision.transforms import transforms
+from torch.utils.data import DataLoader
+
+from dataloader import CustomGolfDB, Normalize, ToTensor
+from flow import get_flows
+
+# class FlowNet(nn.Module):
+#     def __init__(self):
+#         super(FlowNet, self).__init__()
+
+
+if __name__ == '__main__':
+    dataset = CustomGolfDB(
+        video_path='total_videos/',
+        label_path='custom_label/train_label.json',
+        seq_length=64,
+        transform=transforms.Compose([ToTensor(), Normalize(
+            [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+        train=True,
+        input_size=512
+    )
+    data_loader = DataLoader(dataset,
+                             batch_size=12,
+                             shuffle=True,
+                             drop_last=True)
+    dataiter = iter(data_loader)
+    image_batch = next(dataiter)
+    print(image_batch.shape)
+    # get_flows()
